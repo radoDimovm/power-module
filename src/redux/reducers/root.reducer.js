716f719705e3e3199abb902999b6419c9862194f
radoDimovm <rado.dimovm@gmail.com>
2021-06-14 19:43:28 +0300

    Refactor and clean up code.

--- src/redux/reducers/root.reducer.js
+++ src/redux/reducers/root.reducer.js
@@ -1,12 +1,8 @@
 import { combineReducers } from "redux";
 import cameraReducer from "./camera.reducer";
-import commandingPanelReducer from './commandingPanel.reducer';
 import obcReducer from "./obc.reducer";
-import parameterDisplayReducer from './parameterDisplay.reducer';
 
 const rootReducer =  combineReducers({
-  commandingPanel: commandingPanelReducer,
-  parameterDisplay: parameterDisplayReducer,
   obc: obcReducer,
   camera: cameraReducer,
 });
